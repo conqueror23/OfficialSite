149b6060f9198ce1f7144092ef2df510d5028607
bruce <bruce.wang@zerologix.com>
2020-05-06 17:29:11 +1000

    update project structure

--- src/pages/ragudis.js
+++ src/pages/ragudis.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Regulations from '../components/Regulations'
  const ragudis = ({data}) => {
-    const content = data
     return (
         <div>
-            {/* <Regulations lang={'cn'} /> */}
-            {/* Empty here */}
+            <Regulations lang={'cn'} /> */}
         </div>
     )
 }

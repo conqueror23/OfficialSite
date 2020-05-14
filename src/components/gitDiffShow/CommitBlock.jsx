93009c21daf2c5c3a88edd9155967df4d19dfd43
bruce <bruce.wang@zerologix.com>
2020-05-14 19:15:38 +1000

    add version control displays

--- src/components/gitDiffShow/CommitBlock.jsx
+++ src/components/gitDiffShow/CommitBlock.jsx
@@ -4,7 +4,17 @@ import { formatChanges } from "./compareLogic"
 const CommitBlock = ({ change }) => {
   const compare = change.changes
   const compareResult = formatChanges(compare)
-  console.log('show compare result',compareResult)
-  return <div></div>
+  console.log("show compare result", compareResult)
+
+  return (
+    <div>
+      {compareResult.map(line => {
+        line.map((record, index) => {
+            // TODO handle the changes displays logic
+          return index === 0 ? <div className="key">{record}</div> : <div className="value">{record}</div>
+        })
+      })}
+    </div>
+  )
 }
 export default CommitBlock
--- src/components/gitDiffShow/compareLogic.js
+++ src/components/gitDiffShow/compareLogic.js
@@ -1,14 +1,16 @@
 export const formatChangeBody =(changebody)=>{
     const record = {};
+    const recordArray =[]
     const lines =changebody.split('\n');
     lines.map(line=>{
         if(line.includes(':')){
             const lineResult= line.split(':')
+            recordArray.push(lineResult);
             const key = lineResult[0].replace('"').trim();
             record[key]=lineResult[1];
         }
     })
-    return record
+    return recordArray
 }
 export const formatChanges =(changes)=>{
     let changeBody;
